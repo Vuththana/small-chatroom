9edc3d3276a0554698b81de7779b2f48eeb76ef2
Vuththana <vuththanakeo69@gmail.com>
2026-04-24 08:20:47 +0700

    first commit

---
+++ prep/next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['1d68-49-156-35-32.ngrok-free.app', '81a1-36-37-193-97.ngrok-free.ap'],
+}
+export default nextConfig;
